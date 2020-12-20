88b5fcaf7d895ccd597e2471855f8c4f3696ad77
kadim <kad5000@web.de>
2020-12-20 22:16:13 +0100

    ms4 added

---
+++ ms4/ms4-3 (PDF Katalog)/dump_database.bat
@@ -0,0 +1,1 @@
+mysqldump --xml -u root -p -h 127.0.0.1 allgolddb >C:\xampp\htdocs\ms4\"ms4-3 (PDF Katalog)"\dbkatalog.xml
--- pp5/pp5-3 (PDF Katalog)/dump_database.bat
+++
@@ -1,1 +0,0 @@
-mysqldump --xml -u root -p -h 127.0.0.1 itdb >C:\users\fabian\desktop\dump.sql
